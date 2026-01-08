4928b6b30d1ab7b69660d43d6a29b50065fd1262
awdsome217 <awdsome217@gmail.com>
2026-01-07 18:46:37 -0700

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,8 +1,11 @@
-const { Pool } = require("pg");
+import pkg from "pg";
+const { Pool } = pkg;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false,
+  ssl: process.env.DATABASE_URL
+    ? { rejectUnauthorized: false }
+    : false,
 });
 
-module.exports = pool;
+export default pool;
